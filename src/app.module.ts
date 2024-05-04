c534362aae061c368ef1e188d63ef199f0fe616f
yago <yagocam5@gmail.com>
2024-05-03 22:19:38 -0300

    removing user temporaly

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { ProductsController } from './products/products.controller';
 import { ProductsService } from './products/products.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { UserService } from './User/user.service';
-import { UserController } from './User/user.controller';
-
 @Module({
-  imports: [PrismaModule ],
-  controllers: [AppController, ProductsController, UserController],
-  providers: [ ProductsService, UserService],
+  imports: [PrismaModule],
+  controllers: [AppController, ProductsController,],
+  providers: [AppService, ProductsService,],
 })
 export class AppModule {}
